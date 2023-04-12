2d0b9ac293dd7420873a06f6bbfa0f5c7221bfd4
janaka <janakamwm@siu.edu>
2023-04-12 10:54:55 -0500

    separate scheduling actions from shell.c and move it to scheduler.c

--- scheduler.c
+++ scheduler.c
@@ -3,6 +3,11 @@
 #include <stdlib.h>
 #include <sys/types.h>
 #include <wait.h>
+#include <string.h>
+#include "scheduler.h"
+#include "queue.h"
+
+int schtype = FCFS;	//default scheduling type
 
 
 int check_sch_argnum(char *schtype, char *queuenum,int argnum) {
@@ -27,9 +32,67 @@ int check_sch_argnum(char *schtype, char *queuenum,int argnum) {
         return status;
 }
 
-int set_scheduling(char const *input[]) {
+int set_sch_type(char const *input[]) {
         int status = -1;
         printf("\ntest pass\n");
+        if(strcmp(input[0],"fcfs")==0) {
+                schtype = FCFS;
+                status = 1;
+        }
+        else if(strcmp(input[0],"rr")==0){
+                schtype = RR;
+                status = 1;
+        }
+        else if(strcmp(input[0],"mfq")==0){
+                schtype = MFQ;
+                status = 1;
+        }
+        else if(strcmp(input[0],"sjf")==0){
+                schtype = SJF;
+                status = 1;
+        }
+        else {
+                status = -1;
+        }
         status = 1;
         return status;
+}
+
+int get_sch_type(void) {
+        return schtype;
+}
+
+int schedule_task(char *input, struct queue *pid_list, int *fg_pid, int *fg_suspended)
+{
+        int i,t,status;
+	char *args[10];
+	char *temp;
+	struct node *p;
+
+	for (i = 0; i < 10; i++)
+	{
+		args[i]=(char *)malloc(10*sizeof(char));
+	}
+
+	strcpy(args[0],strtok(input,"(,"));
+	for (i=1; (temp=strtok(NULL,",)"))!=NULL; i++) 
+		strcpy(args[i],temp);
+	printf("\n");
+        if (strcmp(args[i-1],"&")==0)
+	{
+		args[i-1]=NULL;
+	}
+	else
+		args[i]=NULL;
+	if ((t=fork())==0)
+	{
+		execv(args[0],args);
+	}
+	enqueue(t,args[0],pid_list);
+	if (args[i-1]!=NULL)
+	{
+		*fg_pid=t;
+		while((*fg_pid)!=0 && (*fg_suspended)!=1)
+			pause();
+	}
 }
--- scheduler.h
+++ scheduler.h
@@ -1,8 +1,16 @@
 #ifndef SCHEDULER_H_
 #define SCHEDULER_H_
 
+#include "queue.h"
+
+#define arrlen(arr) sizeof(arr)/sizeof(arr[0])
+
+typedef enum {FCFS=0, RR, MFQ, SJF}schtype_t;
+
 int check_sch_argnum(char *schtype, char *queuenum,int argnum);
-int set_scheduling(char const *input[]);
+int set_sch_type(char const *input[]);
+int get_sch_type(void);
+int schedule_task(char *input, struct queue *pid_list, int *fg_pid, int *fg_suspended);
 
 #endif /* SCHEDULER_H_ */
 
